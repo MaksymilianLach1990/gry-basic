e586348649da1b76331e01acf79186a89d7e3a9c
Krzysztof Lach <lachkrzysztof56@gmail.com>
2022-04-30 09:22:14 +0200

    Pong OOP - create class for Borad

---
+++ pong_v2/main.py
@@ -0,0 +1,34 @@
+import pygame
+import pygame.locals
+
+class Board(object):
+    """
+    Board for play. Odpowiada za draw game window
+    """
+
+    def __init__(self, width, height):
+        """
+        Konstruktor planszy do gry. Przygotowuje okienko gry.
+
+        :param width:
+        :param height:
+        """
+        self.surface = pygame.display.set_mode((width, height), 0, 32)
+        pygame.display.set_caption("Ping Pong")
+
+    def draw(self, args):
+        """
+        Rysuje okno gry
+        
+        :param args: lista obiektów do narysowania
+        """
+        background = (0, 0, 0)
+        self.surface.fill(background)
+        for drawable in args:
+            drawable.draw_on(self.surface)
+
+        pygame.display.update()
+
+
+board = Board(800, 500)
+board.draw()
